7d8efd2bcf50501dbbdaeaf45e2e8f064fd5202e
Vivre Musicon <vivre.music1@gmail.com>
2024-06-18 09:19:07 +0700

    tes

---
+++ src/component/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import test from "../../src/assets/images/logoL.jpg"
+
+const Test = () => {
+  return (
+    <div>
+        <img src={test} alt="" />
+    </div>
+  )
+}
+
+export default Test
