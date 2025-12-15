3f75dd0d1010a6064c64116f1ee911998a3c1c9d
Vivre Musicon <vivre.music1@gmail.com>
2025-12-15 22:26:06 +0700

    update3

--- src/components/detail/PortoDetailCard.jsx
+++ src/components/detail/PortoDetailCard.jsx
@@ -625,7 +625,7 @@ const ContentWrapper = styled.div`
   }
   @media (max-width: 360px) {
     width: 300rem;
-    height: 650px;
+    height: 700px;
     background-color: #191923;
     padding: 1rem;
     border-radius: 2%;
